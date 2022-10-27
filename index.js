d3a82b35a54ffd51584b6e3b268aad7199cbb252
chamokBarmon <chamokbarmon123@gmail.com>
2022-10-27 12:03:45 +0600

    second-commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cors =require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+const Courses = require('./data/courses.json')
 const program = require('./data/programData.json')
 app.get('/',(req,res)=>{
     res.send ('news data api run') 
+})
+
+app.get('/Courses',(req,res)=>{
+    res.send(Courses)
 })
 app.get('/program',(req,res)=>{
     res.send(program)
